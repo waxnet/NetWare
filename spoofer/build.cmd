beb90dbde4302c9283156cd04c49a1f642185055
waxnet <88239635+waxnet@users.noreply.github.com>
2024-08-23 14:14:41 +0200

    removed old spoofer

--- spoofer/build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-del NetWareSpoofer.exe > nul
-garble -tiny -literals -seed=random build
-rename spoofer.exe NetWareSpoofer.exe > nul
--- spoofer/utilities/console/colors.go
+++
@@ -1,9 +0,0 @@
-package console
-
-// values
-var colorList = map[string]string{
-	"red":    "\033[91m",
-	"green":  "\033[32m",
-	"yellow": "\033[33m",
-	"gray":   "\033[90m",
-}
