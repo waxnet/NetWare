70fee55551e41bc871542a490506682268134da2
waxnet <88239635+waxnet@users.noreply.github.com>
2024-08-27 14:00:49 +0200

    .

--- netware/Configuration/IBindable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace NetWare.Configuration;
-
-public interface IBindable
-{
-    bool Enabled { get; set; }
-    KeyCode? KeyBind { get; set; } // null = currently assigning, stupid approach but whatever
-}
