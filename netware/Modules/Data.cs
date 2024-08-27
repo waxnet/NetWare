046c28224acb0728db5eae52c28e05c3d21faeb3
waxnet <88239635+waxnet@users.noreply.github.com>
2024-08-27 12:04:59 +0200

    .

--- netware/Modules/Data.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetWare.Modules
-{
-    public static class Data
-    {
-        public static string configName = "";
-        public static bool resetFOV = false;
-        public static bool resetZoom = false;
-    }
-}
