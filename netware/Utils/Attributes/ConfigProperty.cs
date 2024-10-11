dbde51060376f3c7504456632b117affeb5eeeaf
waxnet <88239635+waxnet@users.noreply.github.com>
2024-10-11 22:21:06 +0200

    Delete netware directory

--- netware/Utils/Attributes/ConfigProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NetWare.Attributes;
-
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-public sealed class ConfigProperty(string name = null) : Attribute
-{
-    public string Name { get; } = name;
-}
