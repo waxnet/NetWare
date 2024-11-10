bd83ac478a8747180d1362a907a325586928c27b
waxnet <88239635+waxnet@users.noreply.github.com>
2024-11-10 23:00:36 +0100

    Delete netware directory

--- netware/Utils/Attributes/StringReinterpretation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NetWare.Attributes;
-
-[AttributeUsage(AttributeTargets.All)]
-public sealed class StringReinterpretation(string value) : Attribute
-{
-    public string Value { get; set; } = value;
-}
