34835e9e24ae8056790e8065bc792f683600394d
waxnet <88239635+waxnet@users.noreply.github.com>
2024-12-02 17:28:25 +0100

    reupload

---
+++ netware/UI/Styles/ButtonStyles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace NetWare.UI.Styles;
+
+public static class ButtonStyles
+{
+    public static GUIStyle Create()
+    {
+        return new GUIStyle("Box")
+        {
+            normal = {
+                background = Texture.NewBorder(0, 0, 0, .1f, .1f, .1f),
+            },
+
+            hover = {
+                background = Texture.NewBorder(0, 0, 0, .12f, .12f, .12f),
+                textColor = Color.white,
+            },
+
+            border = new RectOffset(1, 1, 1, 1),
+            fontStyle = FontStyle.Bold,
+            fontSize = 13,
+        };
+    }
+}
---
+++ netware/Utils/Attributes/ConfigProperty.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NetWare.Attributes;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public sealed class ConfigProperty(string name = null) : Attribute
+{
+    public string Name { get; } = name;
+}
