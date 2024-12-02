b860c77cad487e30ad612df1b57998c2c7264887
waxnet <88239635+waxnet@users.noreply.github.com>
2024-12-02 17:22:41 +0100

    build mode adjustment

--- launcher/.build/run.cmd
+++ launcher/.build/run.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-nuitka --standalone --onefile --lto=yes --remove-output --enable-plugin=tk-inter --windows-console-mode=disable --windows-icon-from-ico=icon.ico ../main.py
+nuitka --standalone --windows-uac-admin --onefile --lto=yes --remove-output --enable-plugin=tk-inter --windows-console-mode=disable --windows-icon-from-ico=icon.ico ../main.py
